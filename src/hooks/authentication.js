d54f7aec389607cb4f3abf269a3456971b3e89cd
Mehak Nayab <nayabmeh@corp.contoursoftware.net>
2023-03-28 10:56:30 +0500

    authentication

--- src/hooks/authentication.js
+++ src/hooks/authentication.js
@@ -2,13 +2,17 @@ import { useQuery, useMutation } from '@tanstack/react-query'
 import axios from "axios";
 import { useQueryClient } from '@tanstack/react-query'
 
+
+axios.defaults.withCredentials = true;
+
 //login user
 const useLogin = () => {
     return useMutation({
         mutationFn: (data) => {
             return axios
-                .post(`http://localhost:5000/login`, data,{credentials:'include'})
+                .post(`http://localhost:5000/login`, data)
                 .then((res) => res.data)
+                
         },
         onSuccess: async (res) => {
             console.log(res)
